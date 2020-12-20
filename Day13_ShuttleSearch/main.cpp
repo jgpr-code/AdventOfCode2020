be89dd728c7746ef5a070a2fd594fa650ced63ed
Jörg Planner <joerg.planner@methodpark.de>
2020-12-20 02:45:07 +0100

    day13 inefficient solution for part2 (can be improved with chinese remainder)

--- Day13_ShuttleSearch/main.cpp
+++ Day13_ShuttleSearch/main.cpp
@@ -4,33 +4,85 @@
 #include <vector>
 #include <algorithm>
 #include <tuple>
+#include <variant>
 
 using namespace std;
+using ll = long long;
 
-int firstBiggerMultiple(int increment, int target) {
-    int multiple = 0;
-    while (multiple < target) {
+ll firstBiggerMultiple(ll increment, ll target)
+{
+    ll multiple = 0;
+    while (multiple < target)
+    {
         multiple += increment;
     }
     return multiple;
+}
+
+bool isValidTimeForAllBusses(ll time, const vector<tuple<ll, ll>>& busEquations)
+{
+    if (time < 0) return false;
+    for (const auto& t : busEquations) {
+        ll busId = get<0>(t);
+        ll timeOffset = get<1>(t);
+        if ((time + timeOffset) % busId != 0) return false;
+    }
+    return true;
+}
+
+bool isPrime(ll n) {
+    if (n <= 3) {
+        return n > 1;
+    }
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    ll i = 5;
+    while (i * i <= n) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+        i += 6;
+    }
+    return true;
 }
 
 int main()
 {
     string line;
     getline(cin, line);
-    int targetDeparture = stoi(line);
-    tuple<int, int> bestBus(numeric_limits<int>::max(), -1);
+    ll targetDeparture = stoll(line);
+    tuple<ll, ll> bestBus(numeric_limits<ll>::max(), -1);
     getline(cin, line);
     istringstream iss(line);
-    for (string token; getline(iss, token, ',');)
+    string token;
+    vector<tuple<ll, ll>> busses;
+    vector<tuple<ll, ll>> busEquations;
+    for (ll i = 0; getline(iss, token, ','); ++i)
     {
-        if (token == "x") continue;
-        int busId = stoi(token);
-        int busDeparture = firstBiggerMultiple(busId, targetDeparture);
-        bestBus = min(bestBus, make_tuple(busDeparture, busId));
+        if (token == "x")
+        {
+            continue;
+        }
+        ll busId = stoll(token);
+        ll busDeparture = firstBiggerMultiple(busId, targetDeparture);
+        auto bus = make_tuple(busDeparture, busId);
+        bestBus = min(bestBus, bus);
+        busses.push_back(bus);
+        auto busEquation = make_tuple(busId, i);
+        busEquations.push_back(busEquation);
+    }
+    
+    ll waitTime = get<0>(bestBus) - targetDeparture;
+    cout << "Part 1: " << get<1>(bestBus) * waitTime << endl;
+
+    sort(busEquations.begin(), busEquations.end());
+    for (const auto& t : busEquations) {
+        cout << get<0>(t) << "(isPrime: " << isPrime(get<0>(t)) << "), " << get<1>(t) << endl; // all numbers are indeed prime -> chinese remainder
+    }
+    ll increment = get<0>(busEquations.back());
+    ll tryTime = ((100000000000000 / increment) + 1) * increment;
+    while (true) {
+        // cout << "try: " << tryTime << endl;
+        if (isValidTimeForAllBusses(tryTime, busEquations)) break;
+        tryTime += increment;
     }
 
-    int waitTime = get<0>(bestBus) - targetDeparture;
-    cout << "Part 1: " << get<1>(bestBus) * waitTime << endl;
+    cout << "Part 2: " << tryTime << endl;
 }
