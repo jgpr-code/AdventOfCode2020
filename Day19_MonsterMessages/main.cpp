02384e294774d717d9e8cd046abf182f53796689
Jörg Planner <j.planner@gmx.de>
2021-01-07 11:53:18 +0100

    wrong solution part2 (no pure regex solution matches a^nb^m with n > m)

--- Day19_MonsterMessages/main.cpp
+++ Day19_MonsterMessages/main.cpp
@@ -61,7 +61,8 @@ class Ruleset {
  private:
   unordered_map<int, Rule> rules;
 
-  string GetRegexForRule(const Rule& rule) const {
+ public:
+  string GetRegexForRule(const Rule& rule, bool use_part_two) const {
     string regex_str;
     const auto& targets = rule.GetTargets();
     if (targets.size() > 1) regex_str.push_back('(');
@@ -73,17 +74,28 @@ class Ruleset {
         if (part.index() == 0) {  // part is a char
           regex_str.push_back(get<0>(part));
         } else {  // part is a number
-          regex_str.append(GetRegexForRule(rules.at(get<1>(part))));
+          regex_str.append(
+              GetRegexForRule(rules.at(get<1>(part)), use_part_two));
         }
       }
     }
     if (targets.size() > 1) regex_str.push_back(')');
+    if (use_part_two) {
+      switch (rule.GetId()) {
+        case 42:
+        case 31:
+          regex_str.push_back('+');
+          break;
+      }
+    }
     return regex_str;
   }
 
- public:
   void AddRule(const Rule rule) { rules.emplace(rule.GetId(), rule); }
-  string GetRegex() const { return GetRegexForRule(rules.at(0)); }
+  Rule GetRule(int idx) const { return rules.at(idx); }
+  string GetRegex(bool use_part_two) const {
+    return GetRegexForRule(rules.at(0), use_part_two);
+  }
 };
 
 void Test() {
@@ -91,13 +103,19 @@ void Test() {
   string b = "cddc";
   string c = "cdcd";
   string d = "argh";
+  string e = "arghargh";
 
-  string re_str = R"r((abab|cd(cd|dc)|argh))r";
+  string re_str = R"r((abab|cd(cd|dc)|argh){2,})r";
   regex re(re_str);
   cout << a << " matches " << re_str << " ? " << regex_match(a, re) << endl;
   cout << b << " matches " << re_str << " ? " << regex_match(b, re) << endl;
   cout << c << " matches " << re_str << " ? " << regex_match(c, re) << endl;
   cout << d << " matches " << re_str << " ? " << regex_match(d, re) << endl;
+  cout << e << " matches " << re_str << " ? " << regex_match(e, re) << endl;
+
+  string re_2_str = "(argh)+(argh)+";
+  regex re_2(re_2_str);
+  cout << e << " matches " << re_2_str << " ? " << regex_match(e, re_2) << endl;
 
   Rule a_rule("123: \"a\"");
   Rule other_rule("123: 123 456 | 789 987 65 | 3");
@@ -114,14 +132,27 @@ int main() {
     rules.AddRule(move(Rule(line)));
   }
 
-  string re_str = rules.GetRegex();
-  cout << re_str << endl;
-  regex re(re_str);
+  cout << "Rule 42: " << rules.GetRegexForRule(rules.GetRule(42), false)
+       << endl;
+  cout << "Rule 31: " << rules.GetRegexForRule(rules.GetRule(31), false)
+       << endl;
+
+  bool use_part_two = false;
+  string re_str_part1 = rules.GetRegex(use_part_two);
+  use_part_two = true;
+  string re_str_part2 = rules.GetRegex(use_part_two);
+  cout << "regex for part 1:" << endl << re_str_part1 << endl;
+  cout << "regex for part 2:" << endl << re_str_part2 << endl;
+  regex re_part1(re_str_part1);
+  regex re_part2(re_str_part2);
 
   // examine messages
-  int count_valid = 0;
+  int count_valid_part1 = 0;
+  int count_valid_part2 = 0;
   while (getline(cin, line)) {
-    if (regex_match(line, re)) ++count_valid;
+    if (regex_match(line, re_part1)) ++count_valid_part1;
+    if (regex_match(line, re_part2)) ++count_valid_part2;
   }
-  cout << "Part 1: " << count_valid << endl;
+  cout << "Part 1: " << count_valid_part1 << endl;
+  cout << "Part 2: " << count_valid_part2 << endl;
 }
